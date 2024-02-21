4559496de5b0eb63d9db1a4c1f637eb0880dbd82
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-02-21 14:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // When the DOM is loaded, write "DOM load success" on the webpage
   var div = document.createElement("div");
-  var c = "DOM load success";
-  document.body.appendChild(c);
+  div.textContent = "DOM load success";
+  document.body.appendChild(div);
 });
